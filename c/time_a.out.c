9a73de4cfb24203cd0d77e76c7bae6537fc5d60f
localHost <local@host.exe>
2022-08-05 06:50:37 -0400

    .

--- c/time_a.out.c
+++ c/time_a.out.c
@@ -4,15 +4,14 @@
 int main(void){
 
   time_t begin_time, end_time;
-
   begin_time = time(NULL);
 
   int mango;
-  printf("Ingrese un entero: ");
+  printf("\nIngrese un entero: ");
   scanf("%d", &mango); getchar();
 
   end_time = time(NULL);
-  printf("\n\tEl entero es %d  Execution time: %.2f", mango, difftime(end_time, begin_time) );
+  fprintf(stderr, "\n\tEl entero es %d  Execution time: %.2f", mango, difftime(end_time, begin_time) );
 
   return 0;
 }
